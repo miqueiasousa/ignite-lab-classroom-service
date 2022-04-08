d2cdce34166ddff3fe0208095593aafcd202584b
Miqueias Sousa <miqueiassousa80@gmail.com>
2022-04-08 14:40:21 -0300

    feat: add migrate to create table student

---
+++ prisma/migrations/20220408173956_create_table_student/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
